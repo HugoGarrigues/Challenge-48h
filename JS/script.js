2e3d167b4ded76d43084bf2cbbd30af4f4fae9cb
HugoGarrigues <hugo.garrigues@ynov.com>
2023-02-07 12:41:34 +0100

    .

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+document.getElementById('form1').addEventListener('submit', function(){
+    let verif1 = document.getElementById('input1').value;
+    if (verif1 == "AbRic0t") {
+      alert("Bravo, vous avez trouvé le flag : 50 points pour vous !");
+    } else {
+      alert("Dommage, ce n'est pas le bon flag");
+    }
+})
+
+document.getElementById('form2').addEventListener('submit', function(){
+    let verif2 = document.getElementById('input2').value;
+    if (verif2 == "SOS") {
+      alert("Bravo, vous avez trouvé le flag : 50 points pour vous !");
+    } else {
+      alert("Dommage, ce n'est pas le bon flag");
+    }
+})
+
+document.getElementById('form3').addEventListener('submit', function(){
+    let verif3 = document.getElementById('input3').value;
+    if (verif3 == "1654685AE") {
+      alert("Bravo, vous avez trouvé le flag : 50 points pour vous !");
+    } else {
+      alert("Dommage, ce n'est pas le bon flag");
+    }
+})
+
+document.getElementById('form4').addEventListener('submit', function(){
+    let verif4 = document.getElementById('input4').value;
+    if (verif4 == "45azazeaze") {
+      alert("Bravo, vous avez trouvé le flag : 100 points pour vous !");
+    } else {
+      alert("Dommage, ce n'est pas le bon flag");
+    }
+})
+
+
+document.getElementById('form5').addEventListener('submit', function(){
+    let verif5 = document.getElementById('input5').value;
+    if (verif5 == "Disneyland") {
+      alert("Bravo, vous avez trouvé le flag : 150 points pour vous !");
+    } else {
+      alert("Dommage, ce n'est pas le bon flag");
+    }
+})
